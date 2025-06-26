6ac0c69ed5dcd9cb6155a047b14e8dd32bcbc3cc
Alberto  Coppola <alberto.coppola@perenneinvestimentos.com.br>
2025-06-26 12:02:08 -0300

    add cdi

--- dashboard.py
+++ dashboard.py
@@ -13,6 +13,7 @@ st.title("Tracking Fundos")
 # Carrega o CSV com todas as colunas de fundos
 df = pd.read_csv("funds_return.csv", index_col=0)
 df.index = pd.to_datetime(df.index)
+cdi = pd.read_csv("cdi_return.csv", index_col=0, parse_dates=True).sort_index()
 
 # Seleção do fundo via dropdown
 fundos = df.columns.tolist()
@@ -25,7 +26,7 @@ serie = serie[~serie.index.duplicated(keep='first')]
 
 st.subheader(f"Métricas: {fundo_escolhido}")
 
-qs.reports.html(serie, output="relatorio.html", title=f"Relatório - {fundo_escolhido}")
+qs.reports.html(serie, cdi.loc[serie.index], rf=((1+cdi.loc[serie.index]).cumprod()-1)['CDI'][-1],output="meu_relatorio.html",periods_per_year=len(serie.index))
 with open("relatorio.html", "r", encoding="utf-8") as f:
     html = f.read()
 st.components.v1.html(html, height=900, scrolling=True)
